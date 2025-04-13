c7197a9701ca677f68b9b38aa0beaea4a771e93a
Emelianov Artem <yaz008.yaz008@yandex.ru>
2025-04-13 20:48:02 +0300

    [FIX] Owner.Notifications with no notifications

--- src/router/pipelines/owner/notifications/0-notifications.py
+++ src/router/pipelines/owner/notifications/0-notifications.py
@@ -13,6 +13,17 @@ from project.libs.tght import render_file
 from router.instance import router
 
 
+def get_notification(owner: Owner, index: str) -> _Response | None:
+    notifications: list[_Response] = [
+        response_map[response.__sql_id__]
+        for response in responses_table[owner.workhive_id].values()
+        if response_map[response.__sql_id__].status == 'undefined'
+    ]
+    if index != str():
+        index = '0'
+    return notifications[int(index)] if len(notifications) > 0 else None
+
+
 def delete_response(owner: Owner, index: str) -> None:
     if index != str():
         response: _Response = [
@@ -40,19 +51,14 @@ def delete_response(owner: Owner, index: str) -> None:
 def owner_settings(
     owner: Owner, factory: ButtonFactoryClosure, action: str, index: str
 ) -> TGMessage:
-    notifications: list[_Response] = [
-        response_map[response.__sql_id__]
-        for response in responses_table[owner.workhive_id].values()
-        if response_map[response.__sql_id__].status == 'undefined'
-    ]
-    if index != str():
-        index = '0'
-    notification: _Response = notifications[int(index)]
+    notification: _Response | None = get_notification(owner, index)
     match action:
         case 'accept':
+            assert notification is not None
             response_map[notification.response_id].status = 'accepted'
             delete_response(owner, index)
         case 'decline':
+            assert notification is not None
             response_map[notification.response_id].status = 'declined'
             delete_response(owner, index)
         case 'next':
@@ -63,45 +69,66 @@ def owner_settings(
         text=render_file(
             language=owner.language,
             state=(
-                owner.state if len(notifications) > 0 else FSAState.OwnerNoNotifications
+                owner.state
+                if notification is not None
+                else FSAState.OwnerNoNotifications
             ),
-            tag_handlers={
-                'telegram-tag': lambda _: '@no_tag_provided',
-                'worker-name': lambda _: str(
-                    user_table[notification.worker_id].full_name
-                ),
-                'birth-date': lambda _: str(
-                    user_table[notification.worker_id].birth_date
-                ),
-                'point-name': lambda _: str(
-                    points_table[notification.owner_id][
-                        simple_vacancies_table[notification.owner_id][
-                            notification.vacancy_id
-                        ].point_id
-                    ].name
-                ),
-            },
+            tag_handlers=(
+                {
+                    'telegram-tag': lambda _: '@no_tag_provided',
+                    'worker-name': lambda _: str(
+                        user_table[notification.worker_id].full_name
+                    ),
+                    'birth-date': lambda _: str(
+                        user_table[notification.worker_id].birth_date
+                    ),
+                    'point-name': lambda _: str(
+                        points_table[notification.owner_id][
+                            simple_vacancies_table[notification.owner_id][
+                                notification.vacancy_id
+                            ].point_id
+                        ].name
+                    ),
+                }
+                if notification is not None
+                else None
+            ),
         ),
         keyboard=keyboard(
             (
                 RowInfo(factory.saved(WorkHiveButton.Ok))
-                if len(notifications) == 0
+                if notification is None
                 else RowInfo(
                     factory.saved(WorkHiveButton.Accept, args=('accept', index)),
                     factory.saved(WorkHiveButton.Decline, args=('decline', index)),
                 )
             ),
-            RowInfo(
-                (
-                    factory.saved(WorkHiveButton.Back, args=('back', index))
-                    if int(index) > 0
-                    else None
-                ),
-                (
-                    factory.saved(WorkHiveButton.Next, args=('next', index))
-                    if int(index) < len(notifications) - 1
-                    else None
-                ),
+            (
+                RowInfo(
+                    (
+                        factory.saved(WorkHiveButton.Back, args=('back', index))
+                        if int(index) > 0
+                        else None
+                    ),
+                    (
+                        factory.saved(WorkHiveButton.Next, args=('next', index))
+                        if int(index)
+                        < len(
+                            [
+                                response
+                                for response in responses_table[
+                                    owner.workhive_id
+                                ].values()
+                                if response_map[response.__sql_id__].status
+                                == 'undefined'
+                            ]
+                        )
+                        - 1
+                        else None
+                    ),
+                )
+                if notification is not None
+                else None
             ),
         ),
     )
