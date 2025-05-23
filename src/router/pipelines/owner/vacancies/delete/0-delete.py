6645429a8515de93178bf1e49cf3d053aa3d42af
Emelianov Artem <yaz008.yaz008@yandex.ru>
2025-05-23 12:58:25 +0300

    [FIX] Vacancy with responses deletion not working

--- src/router/pipelines/owner/vacancies/delete/0-delete.py
+++ src/router/pipelines/owner/vacancies/delete/0-delete.py
@@ -1,9 +1,31 @@
-from model.tables import simple_vacancies_table
+from uuid import UUID
+
+from model.tables import (
+    _VacancySimple,
+    _Response,
+    simple_vacancies_table,
+    responses_table,
+    response_map,
+)
 from model.types import Owner
 from project.configs import FSAState, FSASymbol, FSAPipeline, WorkHiveButton
 from project.libs.tgdraw import TGMessage, ButtonFactoryClosure, RowInfo, keyboard
 from project.libs.tght import render_file
 from router.instance import router
+
+
+def delete_responses(owner: Owner, ids: list[UUID]) -> None:
+    responses: list[_Response] = [
+        response_map[id.__sql_id__]
+        for id in responses_table[owner.workhive_id].values()
+        if id.__sql_id__ in ids
+    ]
+    for response in responses:
+        responses_table.remove_one(response.owner_id, response.response_id)
+        responses_table.remove_one(response.vacancy_id, response.response_id)
+        # responses_table.remove_one(response.point_id, response.response_id)
+        responses_table.remove_one(response.worker_id, response.response_id)
+        response_map.remove(response.response_id)
 
 
 @router.add(
@@ -18,10 +40,18 @@ def owner_point_payload(
     owner: Owner, factory: ButtonFactoryClosure, index: str
 ) -> TGMessage:
     if index != str():
-        simple_vacancies_table.remove_one(
+        vacancy: _VacancySimple | None = simple_vacancies_table.pop_one(
             owner.workhive_id,
             list(owner.simple_vacancies.values())[int(index)].__sql_id__,
         )
+        if vacancy is not None:
+            response_ids: list[UUID] = [
+                response_id
+                for response_id in responses_table[owner.workhive_id]
+                if response_map[response_id].vacancy_id == vacancy.__sql_id__
+            ]
+            delete_responses(owner, response_ids)
+
     return TGMessage(
         text=render_file(
             language=owner.language,
