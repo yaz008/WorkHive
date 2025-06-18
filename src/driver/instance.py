16eac40558c62d7b60a9babc086ca03838e775f9
Emelianov Artem <yaz008.yaz008@yandex.ru>
2025-06-18 23:40:09 +0300

    Make Subscribe button inline

--- src/driver/instance.py
+++ src/driver/instance.py
@@ -1,3 +1,7 @@
+from typing import cast
+
+from telebot.util import quick_markup
+
 from model.tables import user_table, workhive_id, temp_users, role_table
 from project.configs import TGDriverConfig, ChennelConfig
 from project.core.env import Env
@@ -11,27 +15,43 @@ driver: TGDriver = TGDriver(
     skip_pending=TGDriverConfig.SkipPending,
     threads=TGDriverConfig.Threads,
     is_threaded=TGDriverConfig.IsThreaded,
-    on_session_expiration=lambda session: driver.send_message(
-        chat_id=session.telegram_id,
-        text=render_file(
-            language=(
-                user_table[workhive_id[session.telegram_id].value].language
-                if session.telegram_id not in temp_users
-                else temp_users[session.telegram_id].value['language']
-            ),
-            state='driver-on-session-expiration',
-            tag_handlers={
-                'channel': lambda _: (
-                    f'Подпишитесь на наш канал: {(
-                        ChennelConfig.WorkersChannelLink
-                        if role_table[workhive_id[session.telegram_id].value].role
-                        == 'worker'
-                        else ChennelConfig.OwnersChannelLink
-                    )}'
+    on_session_expiration=lambda session: cast(
+        None,
+        driver.send_message(
+            chat_id=session.telegram_id,
+            text=render_file(
+                language=(
+                    user_table[workhive_id[session.telegram_id].value].language
                     if session.telegram_id not in temp_users
-                    else str()
-                )
-            },
+                    else temp_users[session.telegram_id].value['language']
+                ),
+                state='driver-on-session-expiration',
+                tag_handlers={
+                    'channel': lambda _: (
+                        f'Подпишитесь на наш канал: {(
+                            ChennelConfig.WorkersChannelLink
+                            if role_table[workhive_id[session.telegram_id].value].role
+                            == 'worker'
+                            else ChennelConfig.OwnersChannelLink
+                        )}'
+                        if session.telegram_id not in temp_users
+                        else str()
+                    )
+                },
+            ),
+            reply_markup=quick_markup(
+                values={
+                    'Подпишись на наш канал!': {
+                        'url': (
+                            ChennelConfig.WorkersChannelLink
+                            if role_table[workhive_id[session.telegram_id].value].role
+                            == 'worker'
+                            else ChennelConfig.OwnersChannelLink
+                        )
+                    }
+                },
+                row_width=1,
+            ),
         ),
     ),
 )
