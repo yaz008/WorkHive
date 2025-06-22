0f7fff769aded0f6fedb9546fc685c315244db98
Emelianov Artem <yaz008.yaz008@yandex.ru>
2025-06-22 22:56:21 +0300

    [FIX] Point link parsed incorrectly

--- src/router/pipelines/owner/points/add/0-address.py
+++ src/router/pipelines/owner/points/add/0-address.py
@@ -26,7 +26,10 @@ def get_temp_point(telegram_id: int) -> TempPoint:
 
 
 def get_link(string: str) -> str | None:
-    links: list[str] = findall(pattern=r'https://[\w$%&?#@!\/\+\-\*\.]+', string=string)
+    links: list[str] = findall(
+        pattern=r'https://[\w=$%&?#@!\/\+\-\*\.]+',
+        string=string,
+    )
     return links[0] if len(links) > 0 else None
 
 
