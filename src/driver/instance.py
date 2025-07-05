e155e50b5bbb41fac9a7b76bb74acd716ca81024
Emelianov Artem <yaz008.yaz008@yandex.ru>
2025-07-05 11:47:36 +0300

    Update driver-on-owner-session-expiration

--- src/driver/instance.py
+++ src/driver/instance.py
@@ -4,9 +4,8 @@ from telebot.types import InlineKeyboardMarkup
 from telebot.util import quick_markup
 
 from model.tables import user_table, workhive_id, temp_users, role_table, state_table
-from project.configs import TGDriverConfig, ChennelConfig, FSASymbol, FSAState
+from project.configs import TGDriverConfig, ChennelConfig, FSAState
 from project.core.env import Env
-from project.libs.fsa import serializer
 from project.libs.tgdraw import TGDriver
 from project.libs.tght import render_file
 
@@ -38,18 +37,6 @@ def get_keyboard(telegram_id: int) -> InlineKeyboardMarkup | None:
             return quick_markup(
                 values={
                     'Подпишись на наш канал!': {'url': ChennelConfig.OwnersChannelLink},
-                    'Подбор персонала': {
-                        'callback_data': serializer.serialize(
-                            state=state,
-                            symbol=FSASymbol.Vacancies,
-                        )
-                    },
-                    'Как это работает?': {
-                        'callback_data': serializer.serialize(
-                            state=state,
-                            symbol=FSASymbol.AboutProject,
-                        )
-                    },
                 },
                 row_width=1,
             )
